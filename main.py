4397a3fb0da214b99bfeea5c7919228c6d05d021
Amirhossein Nouranizadeh <amirhossein.nouranizadeh@gmail.com>
2025-05-25 17:07:15 +0330

    updated.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from model import DDPM
 def main():
     # Generate dataset
     X, y, pdf_values = make_gaussian_mixture(
-        n_samples=1000,
+        n_samples=10_000,
         n_features=2,
         means=np.array([[-5, -5], [0, 5], [5, -2]]),
         covariances=np.array([[[2, 0], [0, 2]],
@@ -26,16 +26,40 @@ def main():
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    dataloader = DataLoader(dataset=TensorDataset(torch.from_numpy(X)),
+    dataloader = DataLoader(dataset=TensorDataset(torch.from_numpy(X).to(torch.float32)),
                             batch_size=128,
                             shuffle=True)
 
     model = DDPM(input_dim=2,
                  hidden_dim=32,
-                 T=1000,
+                 T=200,
                  beta_start=1e-4,
                  beta_end=0.02).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+    # train
+    epochs = 5_000
+    for epoch in range(1, epochs + 1):
+        total_loss = 0.0
+        for (batch,) in dataloader:
+            batch = batch.to(device)
+            loss = model(batch)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        print(f"Epoch {epoch} - Loss: {total_loss:.4f}")
+
+    test_samples = model.sample(num_samples=1000, device=device).cpu().numpy()
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    scatter = plt.scatter(test_samples[:, 0], test_samples[:, 1], alpha=0.6)
+    plt.colorbar(scatter, label='Class')
+    plt.title(f'Generated samples')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.show()
 
     # # Visualize the data
     # plt.figure(figsize=(12, 5))
